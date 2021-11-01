b5c57836a7e6230e6561ea834c0052356f1329cd
GordDar <92335038+GordDar@users.noreply.github.com>
2021-11-01 16:46:09 +0300

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,49 @@
+from tkinter import *
+import random, time
+
+def push(b):
+    global game
+    global game_left
+    global turn
+    game[b] = 'X'
+    buttons[b].config(text='X', bg='white', state='disabled')
+    game_left.remove(b)
+    if b==4 and turn==0:
+        t = random.choice(game_left)
+    elif b!=4 and turn==0:
+        t=4
+    if turn>0:
+        t=8-b
+        if t not in game_left:
+            t= random.choice(game_left)
+    game[t] = '0'
+    time.sleep(0.5)
+    buttons[t].config(text='0', bg='white', state='disabled')
+    turn += 1
+
+
+
+
+game=[None]*9
+game_left=list(range(9))
+turn=0
+
+
+root = Tk()
+label=Label(width=20, text='Игра крестики-нолики', font=('Arial', 20, 'bold'))
+
+buttons=[Button(width=5, height=2, font=('Arial', 28, 'bold'), bg='green', command=lambda x=i: push(x)) for i in range(9)]
+
+label.grid(row=0, column=0, columnspan=3)
+row=1
+column=0
+for i in range(9):
+    buttons[i].grid(row=row, column=column)
+    column+=1
+    if column==3:
+        row+=1
+        column=0
+
+
+root.mainloop()
+
